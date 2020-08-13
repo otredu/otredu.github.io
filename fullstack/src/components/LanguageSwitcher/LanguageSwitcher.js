f6792f14869905f9676e5a9233b00283529b5044
Tiina Partanen <tiina.partanen@edu.tampere.fi>
2020-08-13 21:14:30 +0300

    fixed images

---
+++ fullstack/src/components/LanguageSwitcher/LanguageSwitcher.js
@@ -0,0 +1,39 @@
+import './LanguageSwitcher.scss';
+
+import PropTypes from 'prop-types';
+import React from 'react';
+import { Link } from 'gatsby';
+
+const Language = ({ language, active }) => {
+  return language === active ? (
+    <span className="language-switcher__active-language">{language}</span>
+  ) : (
+    <div className="language-switcher__language">
+      <Link to={language === 'en' ? '/en' : language === 'zh' ? '/zh' : '/'}>
+        {language}
+      </Link>
+    </div>
+  );
+};
+
+const LanguageSwitcher = ({ lang }) => {
+  return (
+    <div className="language-switcher">
+      <Language language="fi" active={lang} />
+
+      {/* <Language language="en" active={lang} />
+
+      <Language language="zh" active={lang} /> */}
+    </div>
+  );
+};
+
+LanguageSwitcher.defaultProps = {
+  lang: 'fi',
+};
+
+LanguageSwitcher.propTypes = {
+  lang: PropTypes.string.isRequired,
+};
+
+export default LanguageSwitcher;
---
+++ fullstack/src/templates/partColors.js
@@ -0,0 +1,12 @@
+export const partColors = {
+  0: 'light-violet',
+  1: 'green',
+  2: 'dark-orange',
+  3: 'light-orange',
+  4: 'yellow',
+  5: 'pink',
+  6: 'violet',
+  7: 'light-blue',
+  8: 'turquoise',
+  9: 'light-green'
+};
